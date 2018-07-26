fd63eff744f91a8608650c0cfce757d332a16191
AT <korail@protonmail.com>
2018-07-27 00:49:51 +0200

    Update varlist_view.R

--- R/varlist_view.R
+++ R/varlist_view.R
@@ -18,14 +18,16 @@ varlist.view <- function(d) {
     variableslabel <- as.character(variableslabel)
     variableslabel[variableslabel == "NULL"] <- NA
     label <- (label = variableslabel)
-    class <- as.list(sapply(d, class))
+    cl <- as.list(sapply(d, class))
     type <- as.list(sapply(d, typeof))
     valid <- as.list(sapply(d, function(x) length(x) - sum(is.na(x))))
     nas <- as.list(sapply(d, function(x) sum(is.na(x))))
     options(scipen = 999)
     values <- lapply(d, function(x) if (is.factor(x)) {
         paste(levels(x), collapse = ", ")
-    } else if (is.logical(x)) {
+    } else if (is.logical(x) & (length(x) == sum(is.na(x)))) {
+        "Full NA"
+    } else if (is.logical(x) & (length(x) > sum(is.na(x)))) { 
         paste(round(sum(x, na.rm = T)/n * 100), "% TRUE", sep = "")
     } else if (is.character(x)) {
         NA
@@ -37,7 +39,7 @@ varlist.view <- function(d) {
     varlist <- data.frame(name)
     varlist$Label <- label
     varlist$Values <- values
-    varlist$Class <- class
+    varlist$Class <- cl
     varlist$Type <- type
     varlist$Valid <- valid
     varlist$NAs <- nas
