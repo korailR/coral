491d9413ee6c0ad22578b311a5cd7964c66647cd
AT <korail@protonmail.com>
2019-01-17 01:57:30 +0100

    Update varlist.R

--- R/varlist.R
+++ R/varlist.R
@@ -7,42 +7,44 @@
 #'
 #' @examples#' \dontrun{
 #' varlist(df) # df is a data frame
+#' varlist(df, tdf = TRUE) # if tdf = TRUE, print a tibble data format 
 #' }
-varlist <- function(d) {
+varlist <- function(d, tdf = FALSE) {
   myfunc.label <- function(x) attributes(x)[["label"]]
   variableslabel <- sapply(d, myfunc.label)
-  name <- names(variableslabel)
-  variableslabel <- as.character(variableslabel)
-  variableslabel[variableslabel == "NULL"] <- NA
-  label <- (label = variableslabel)
-  class <- as.list(sapply(d, class))
-  type <- as.list(sapply(d, typeof))
-  valid <- as.list(sapply(d, function(x) length(x) - sum(is.na(x))))
-  nas <- as.list(sapply(d, function(x) sum(is.na(x))))
+  Names <- names(variableslabel)
+  varlist <- as.data.frame(Names)
+  varlist$Label <- as.character(variableslabel)
+  varlist$Label[varlist$Label == "NULL"] <- NA
   options(scipen = 999)
-  values <- lapply(d, function(x) if (is.factor(x)) {
+  Values <- lapply(d, function(x) if (is.factor(x)) {
     paste(levels(x), collapse = ", ")
-  } else if (is.logical(x) & (length(x) == sum(is.na(x)))) {
-    "Full NA"
-  } else if (is.logical(x) & (length(x) > sum(is.na(x)))) {
-    paste(round(sum(x, na.rm = T)/n * 100), "% TRUE", sep = "")
-  } else if (is.character(x)) {
-    NA
-  } else if (all(is.na(x))) {
-    "Full NA"
-  } else if (is.POSIXct(x) | is.POSIXlt(x) | is.POSIXt(x) | is.Date(x)) {
-    paste(min(x, na.rm = T), "...", max(x, na.rm = T))
+    } else if (is.logical(x) & (length(x) == sum(is.na(x)))) {
+      "Full NA"
+      } else if (is.logical(x) & (length(x) > sum(is.na(x)))) {
+        paste(round(sum(x, na.rm = T)/n * 100), "% TRUE", sep = "")
+        } else if (is.character(x)) {NA
+          } else if (all(is.na(x))) {
+            "Full NA"
+            } else if (is.POSIXct(x) | is.POSIXlt(x) | is.POSIXt(x) | is.Date(x)) {
+              paste(min(x, na.rm = T), "...", max(x, na.rm = T))
+              } else {
+                paste(round(min(x, na.rm = T), digits = 4), "...", round(max(x, na.rm = T), digits = 4))
+                })
+  Class <- lapply(d, function(x) if (is.POSIXt(x) | is.POSIXct(x) | is.POSIXlt(x)) {
+    paste(class(x), collapse = ", ")
   } else {
-    paste(round(min(x, na.rm = T), digits = 4), "...", round(max(x, na.rm = T), digits = 4))
+    class(x)
   })
-  varlist <- data.frame(name)
-  varlist$Label <- label
-  varlist$Values <- values
-  varlist$Class <- lapply(d, function(x) ifelse(is.POSIXt(x) | is.POSIXct(x) | is.POSIXlt(x),
-                                                paste(class(d$startdate), collapse = ", "), class))
-  varlist$Type <- type
-  varlist$Valid <- valid
-  varlist$NAs <- nas
+  Type <- sapply(d, typeof)
+  
+  varlist$Values <- Values
+  varlist$Class <- Class
+  varlist$Type <- Type
+  varlist$Valid <- apply(d, 2, function(x) length(x) - sum(is.na(x)))
+  varlist$NAs <- apply(d, 2, function(x) sum(is.na(x)))
   varlist <- as.data.frame(lapply(varlist, unlist))
-  View(varlist, paste("varlist", deparse(substitute(d)), sep = " "))
-}
+  varlist <- as_tibble(varlist)
+  
+  ifelse(tdf ,return(varlist), return(View(varlist, paste("varlist", deparse(substitute(d)), sep = " "))))
+  }
