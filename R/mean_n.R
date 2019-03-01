3abc6016277f39e099fadf47e05a915ee77340f4
AT <korail@protonmail.com>
2019-03-01 12:26:15 +0100

    Update mean_n.R

--- R/mean_n.R
+++ R/mean_n.R
@@ -1,16 +1,21 @@
-#' Computing mean over variables and dealing with NAs
+#' Computing row means from a `data.frame` or `matrix` and dealing with NAs
 #'
-#' @param vars variables (columns) of a data.frame
-#' @param not.na the minimum number of valid answers (not NA)
+#' @param x a data frame or matrix
+#' @param n a numeric value indicates the minimum amount of valid values (not NA) per row to calculate row mean (default = 1). Numeric value between 0 and 1 indicates a proportion of valid values per row to calculate the row mean
 #'
-#' @return mean
+#' @return row means
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' mean_n(df[2:5,3])
+#' mean_n(df[2:5, 3])
+#' mean_n(df[1:5, 0.5])
 #' }
-mean_n <- function(vars, not.na = 0) {
-    apply(vars, 1, function(x) ifelse(sum(!is.na(x)) >= not.na, mean(x, na.rm = TRUE),
-        NA))
-}
+mean_n <- function(x, n = 1) {
+  digs <- n%%1
+  if (digs != 0) {n <- round(ncol(x) * digs)}
+  if (ncol(x) < n) {
+    warning("'n' must be smaller or equal to number of columns in data frame or matrix.", immediate. = T)}
+    apply(x, 1, function(x) ifelse(sum(!is.na(x)) >= n, mean(x, na.rm = TRUE),
+                                    NA))
+  }
