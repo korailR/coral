0d6b260ee6fe3acbec02ed27449c3c25490c7a5d
AT <korail@protonmail.com>
2020-04-24 03:47:55 +0200

    Implement multiples classes

--- R/varlist.R
+++ R/varlist.R
@@ -34,7 +34,7 @@ varlist <- function(x, tdf = FALSE) {
                 })
   Class <- lapply(x, function(x) if (lubridate::is.POSIXt(x) | lubridate::is.POSIXct(x) | lubridate::is.POSIXlt(x)) {
     paste(class(x), collapse = ", ")
-  } else if (is.ordered(x) | is.factor(x)){
+  } else if (length(class(x)) > 1){
     paste(class(x), collapse = ", ")
   } else {
     class(x)
