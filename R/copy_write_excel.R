e6c7a117ec87ee5e3ddf7fb84ade83ed8375f93a
koko <30215504+korailR@users.noreply.github.com>
2022-12-31 14:22:01 +0100

    Update copy_write_excel.R

--- R/copy_write_excel.R
+++ R/copy_write_excel.R
@@ -1,7 +1,11 @@
-#' Copy data frame from View to Excel
+#' Copy in the clipboard a data frame from View's RStudio to Excel
 #'
+#' @param x a data frame or matrix
 #'
+#' @examples
+#' \dontrun{
 #' copy_write_excel(x)
+#' }
 
 copy_write_excel <- function(x,row.names=FALSE,col.names=TRUE,...) {
   write.table(x,"clipboard-1000000",sep="\t",row.names=row.names,col.names=col.names,...)}
